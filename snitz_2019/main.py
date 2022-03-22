6efbb26d9d167b95baaca7fe57fd85b144a2b457
Travis Gould <52669531+TGouldBates@users.noreply.github.com>
2022-03-22 16:31:29 -0400

    Snitz 2019 revision 2
    Added Jupyter Notebook to clean odorants_raw.csv and add CID, CAS lists for mixtures; incorporated Rick's 2nd round of edits.

--- snitz_2019/main.py
+++ snitz_2019/main.py
@@ -2,7 +2,8 @@
 # coding: utf-8
 
 # Full reproducability is dependent on published data files to be downloaded and saved to ./published_data
-# Note: odorants.csv had to be manually created by copy/pasting from Table 1 in online version of manuscript
+# odorants_raw.csv was manually created by copy/pasting from Table 1 in online version of manuscript
+# complete_odorants_csv.ipynb must then be run to clean odorants_raw.csv and add CID, CAS lists for mixtures
 
 import pandas as pd
 import numpy as np
@@ -10,17 +11,16 @@ import re
 import pyrfume
 
 # info on odorants used in this study
-# I had to manually add CID for IPM (the diluent)
-odorants = pd.read_csv('published_data\\odorants.csv', index_col=0).dropna(subset=['CID'])
-
-# mapping for odor codes to CID
-codeToCID = dict(zip(odorants['Odor code'].str.split().str.join(''),odorants['CID'].astype('int')))
-
+odorants = pd.read_csv('published_data\\odorants.csv', index_col=0)      
 odorants.head()
 
 # get info from cids
-# drop rows with no CID since these represent mixtures
-cids = odorants['CID'].dropna().astype('int').tolist()
+
+# mapping for odor codes to CID
+codeToCID = dict(zip(odorants.index, odorants['CID']))
+
+# remove mixtures
+for key in [key for key in codeToCID if len(codeToCID[key].split(',')) > 1]: del codeToCID[key]                   
 info_dict = pyrfume.from_cids(cids)
 
 # create dataframe for molecules.csv
@@ -35,19 +35,18 @@ df1.head()
 # SmellSpace data -> behavior_1.csv
 odorIdx1 = [df1.columns.get_loc(col) for col in df1 if col.startswith('Odor')]
 terms = [re.sub(r'\d+$', '', x) for x in list(df1)[odorIdx1[0]+1:odorIdx1[0]+14]]
-colNames1 = ['CID', 'UID', 'OdorCode'] + terms
+colNames1 = ['UID', 'OdorCode'] + terms
 
 data_dict1 = {}
 n = 0
 for row in df1.to_numpy():
     for i in odorIdx1:
-        if row[i] in codeToCID: # skip codes indicating mixtures
-            idx = [1] + [k for k in range(i,i+14)]
-            data_dict1[n] = [codeToCID[row[i]]] + [row[k] for k in idx]
-            n += 1
+        idx = [1] + [k for k in range(i,i+14)]
+        data_dict1[n] = [row[k] for k in idx]
+        n += 1
 
-behav1 = pd.DataFrame.from_dict(data_dict1, orient='index', columns=colNames1).set_index(['CID', 'UID']).sort_index()
-behav1.drop(['OdorCode'], axis=1).head()
+behav1 = pd.DataFrame.from_dict(data_dict1, orient='index', columns=colNames1).set_index(['OdorCode', 'UID']).sort_index()
+behav1.head()
 
 # data from in-lab control group
 df2 = pd.read_excel('published_data\\bjz014_suppl_supplementary_datafile2.xlsx', engine='openpyxl')
@@ -55,7 +54,6 @@ df2 = pd.read_excel('published_data\\bjz014_suppl_supplementary_datafile2.xlsx',
 # Add user id's
 df2.insert(0, 'UID', np.repeat(np.arange(len(df2)/2),2))
 df2['UID'] = df2['UID'].astype(int)
-
 df2.head()
 
 # Control group data -> behavior.2.csv
@@ -67,7 +65,7 @@ n = 0
 for row in df2.to_numpy():
     for i in odorIdx2:
         idx = [0,3] + [k for k in range(i,i+14)]
-        data_dict2[n] = [codeToCID[row[i]]] + [row[k] for k in idx]
+        data_dict2[n] = [int(codeToCID[row[i]])] + [row[k] for k in idx]
         n += 1
 
 behav2 = pd.DataFrame.from_dict(data_dict2, orient='index', columns=colNames2).set_index(['CID', 'UID', 'SessionNumber']).sort_values(by=['CID', 'UID', 'SessionNumber'])
@@ -85,7 +83,6 @@ subjects.head()
 
 # write to disk
 molecules.to_csv('molecules.csv')
-behav1.drop(['OdorCode'], axis=1).to_csv('behavior_1.csv')
+behav1.to_csv('behavior_1.csv')
 behav2.drop(['OdorCode'], axis=1).to_csv('behavior_2.csv')
 subjects.to_csv('subjects.csv')
-
