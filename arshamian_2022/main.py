344c96b5912e3a41aed032225c4f26eb7bcfd051
Travis Gould <52669531+TravisGould@users.noreply.github.com>
2022-08-10 16:49:19 -0400

    standardization updates

--- arshamian_2022/main.py
+++ arshamian_2022/main.py
@@ -24,7 +24,7 @@ molecules = pd.DataFrame(info_dict).set_index('CID').sort_index()
 molecules.head()
 
 # Universal pleasantness data -> behavior_1.csv
-behav1 = pd.read_csv('data\\Universal_Pleasantness.csv', index_col=0)
+behav1 = pd.read_csv('Universal_Pleasantness.csv', index_col=0)
 behav1.head()
 
 # Create unique ParticipantID for each Participant/Group
@@ -32,13 +32,14 @@ participants = sorted(list(set(list(zip(behav1['Group'], behav1['Participant'], 
 to_pid = {(tup[0], tup[1]): i+1 for i, tup in enumerate(participants)}
 
 # map OdorName to CID, Participant/Group to ParticipantID, and re-index
-behav1['CID'] = behav1.apply(lambda row: name_to_cid[row['OdorName']], axis=1)
+behav1['Stimulus'] = behav1.apply(lambda row: name_to_cid[row['OdorName']], axis=1)
 behav1['ParticipantID'] = behav1.apply(lambda row: to_pid[(row['Group'], row['Participant'])], axis=1)
-behav1 = behav1.set_index(['CID','ParticipantID']).sort_index()
+behav1 = behav1.set_index(['Stimulus','ParticipantID']).sort_index()
+behav1.drop(['OdorName', 'Group', 'Participant', 'Subsistence'], axis=1, inplace=True)
 behav1.head()
 
 # All data intensity -> behavior_2.csv
-behav2 = pd.read_csv('data\\All_data_intensity.txt', sep='\t')
+behav2 = pd.read_csv('All_data_intensity.txt', sep='\t')
 
 # Simplify column names & replace Participant/Group with ParticipantID
 behav2.columns = ['Participant', 'Group', 'Rank 1', 'Rank 2', 'Rank 3', 'Rank 4', 'Rank 5', 'Rank 6', 'Rank 7', 'Rank 8', 'Rank 9', 'Rank 10']
@@ -53,7 +54,8 @@ for col in behav2.columns[2:]:
 behav2.head()    
 
 # Mainland lab ranking -> behavior_3.csv
-behav3 = pd.read_csv('data\\UniversalPleasantnessMainlandLabRanking.csv')
+behav3 = pd.read_csv('UniversalPleasantnessMainlandLabRanking.csv')
+behav3.rename(columns={'CID': 'Stimulus'}, inplace=True)
 behav3.head()
 
 # convert rater names to raterID for anonymization
@@ -64,7 +66,7 @@ for i, rater in enumerate(raters):
     rater_to_id[rater] = i + offset
 
 behav3['ParticipantID'] = behav3.apply(lambda row: rater_to_id[row['Rater']], axis=1)
-behav3 = behav3.set_index(['CID','ParticipantID']).sort_index()
+behav3 = behav3.set_index(['Stimulus','ParticipantID']).sort_index()
 behav3.head()
 
 # Dataframe for subjects.csv
@@ -78,9 +80,15 @@ subjects = pd.DataFrame.from_dict(subj_dict, orient='index', columns=['Group', '
 subjects.index.names = ['ParticipantID']
 subjects.head()
 
+# Dataframe for stimuli.csv; all stimuli are CIDs
+stimuli = pd.DataFrame(molecules.index, index=molecules.index.tolist())
+stimuli.index.name = 'Stimulus'
+stimuli.head()
+
 # write to disk
 molecules.to_csv('molecules.csv')
 subjects.to_csv('subjects.csv')
-behav1.drop(['OdorName', 'Group', 'Participant', 'Subsistence'], axis=1).to_csv('behavior_1.csv')
+behav1.to_csv('behavior_1.csv')
 behav2.drop(['Group', 'Participant'], axis=1).to_csv('behavior_2.csv')
 behav3.drop(['Rater', 'OdorKey', 'OdorName'], axis=1).to_csv('behavior_3.csv')
+stimuli.to_csv('stimuli.csv')
