3b6fc61b2212058814bb59d1b9f85bf4b6197db7
Travis Gould <52669531+TravisGould@users.noreply.github.com>
2022-08-11 19:38:12 -0400

    standardization edits

--- ma_2021/main.py
+++ ma_2021/main.py
@@ -1,23 +1,11 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.10.3
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
 import pandas as pd
 from pyrfume.odorants import get_cids, from_cids
 
-odor_info = pd.read_excel('data in brief V2.xlsx', sheet_name='odor information')
-ratings = pd.read_excel('data in brief V2.xlsx', sheet_name='individual data')
+odor_info = pd.read_excel('data_in_brief_V2.xlsx', sheet_name='odor information')
+ratings = pd.read_excel('data_in_brief_V2.xlsx', sheet_name='individual data')
 
 cas = odor_info['CAS.']
 cids = get_cids(cas.values)
@@ -27,22 +15,26 @@ odor_info = odor_info.set_index('Odorant') # For easy map/apply lookup
 odor_info = odor_info.rename(index={'ethyl 3-(methylsulfanyl)propan oate': 'ethyl 3-(methylsulfanyl)propanoate'})
 
 molecules = pd.DataFrame(from_cids(list(cids.values()))).set_index('CID').sort_index()
-molecules.to_csv('molecules.csv')
 molecules.head()
 
-mixtures = odor_info.set_index('CID')[['Cons.(mg/mL)', 'Solvent', 'Purity']].sort_index()
-mixtures = mixtures.rename(columns={'Cons.(mg/mL)': 'Concentration (mg/mL)'})
-mixtures.to_csv('mixtures.csv')
-mixtures.head()
+stimuli = odor_info[['CID', 'Cons.(mg/mL)', 'Solvent', 'Purity']]
+stimuli = stimuli.rename(columns={'Cons.(mg/mL)': 'Concentration (mg/mL)'})
+stimuli.index = stimuli.CID
+stimuli.index.name = 'Stimulus'
+stimuli.sort_index(inplace=True)
+stimuli.head()
 
 ratings['CID A'] = ratings['odor A'].apply(odor_info['CID'].xs)
 ratings['CID B'] = ratings['odor B'].apply(odor_info['CID'].xs)
 ratings['Repeat'] = ratings['Repeat'].fillna(1).astype(int)
-ratings = ratings.rename(columns={'Sub.': 'Subject',
-                                  'Repeat': 'Rep'})
+ratings = ratings.rename(columns={'Sub.': 'Subject', 'Repeat': 'Rep', 'CID A': 'Stimulus A', 'CID B': 'Stimulus B'})
 ratings.columns = ratings.columns.map(str.strip)
 behavior = ratings.drop(['Trial', 'R-Trial', 'odor A', 'odor B'], axis=1)
-behavior = behavior.set_index(['CID A', 'CID B', 'Subject', 'Rep']).sort_index()
-behavior = behavior
-behavior.to_csv('behavior.csv')
+behavior = behavior.set_index(['Stimulus A', 'Stimulus B', 'Subject', 'Rep']).sort_index()
 behavior.head()
+
+# Write to disk
+molecules.to_csv('molecules.csv')
+stimuli.to_csv('stimuli.csv')
+behavior.to_csv('behavior.csv')
+
