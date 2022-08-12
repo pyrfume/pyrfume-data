69a2916d44ba0aad14ad030c593ef80d1a641b3d
Travis Gould <52669531+TravisGould@users.noreply.github.com>
2022-08-11 21:37:12 -0400

    standardization edits

--- mainland_2015/main.py
+++ mainland_2015/main.py
@@ -1,19 +1,5 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.10.3
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
-
-# # Processing pipeline for Mainland et al, 2015
+#!/usr/bin/env python
+# coding: utf-8
 
 import pandas as pd
 import pyrfume
@@ -21,24 +7,55 @@ from pyrfume import get_cids
 from pyrfume import from_cids
 
 odorants = pd.read_csv('Odors.tsv', sep='\t')
-molecules = pd.DataFrame(from_cids(odorants[~odorants.CID.isna()]['CID'].astype(int))) #Retrieve Pubchem informations from CID
+odorants.head()
 
-molecules = molecules.merge(odorants, on='CID', how="outer") #Merge on CID, outer merge if mixtures are wanted
+# Try to fetch CIDs first with SMILES
+cids = pd.Series(get_cids(odorants['SMILES'], kind='SMILES'))
 
-molecules = molecules.drop(['SMILES'], axis=1).drop_duplicates(subset=['Odor'])
-molecules = molecules.rename(columns={'Odor': 'Odorant'}).set_index("CID") #Change column name for consistency
-molecules.to_csv('molecules.csv')
+# Add CIDs to odorants dataframe
+odorants['CID'] = odorants['SMILES'].apply(cids.xs).astype(int)
 
-receptors = pd.read_csv('Receptors.tsv', sep='\t')
-receptors.set_index('Gene')
-receptors.to_csv('receptors.csv')
+# Missing CIDs appear to be substances; assign negative numbers for CID
+missing = odorants[odorants.CID == 0].CID.to_frame()
+missing.CID = range(-1, -(missing.shape[0] + 1), -1)
+
+odorants.loc[missing.index, 'CID'] = missing.CID
+odorants = odorants.set_index('CID').sort_index()
+odorants.head()
+
+molecules = pd.DataFrame(from_cids(odorants['CID'])).set_index('CID').sort_index()
+
+molecules.head()
+
+# Odor #'s are unique, use as stimulus IDs
+stimuli = odorants.drop('SMILES', axis=1).reset_index().copy()
+stimuli.rename(columns={'Odor': 'Stimulus', 'CASRegistryNum': 'CASNo'}, inplace=True)
+stimuli = stimuli.set_index('Stimulus').sort_index()
+stimuli.head()
+
+# Use OR # as Subject ID
+subjects = pd.read_csv('Receptors.tsv', sep='\t')
+subjects.index = subjects.OR
+subjects.index.name = 'Subject'
+subjects.head()
 
 screening = pd.read_csv('PrimaryScreen.tsv', sep='\t')
-screening['OR'] = screening['OR'].fillna(0).astype(int)
-screening = screening.rename(columns={'Odor': 'Odorant'})
-screening.to_csv('behavior_1.csv')
+screening.dropna(subset='Odor', axis=0, inplace=True) # Remove rows with no data
+screening = screening.rename(columns={'Odor': 'Stimulus', 'OR': 'Subject'})
+screening[['Stimulus', 'Subject']] = screening[['Stimulus', 'Subject']].astype(int)
+screening = screening.set_index(['Stimulus', 'Subject']).sort_index()
+screening.head()
 
 data = pd.read_excel('sdata20152-s2.xls')
+data = data.rename(columns={'Odor': 'Stimulus', 'OR': 'Subject'})
+data = data.set_index(['Stimulus', 'Subject']).sort_index()
 
-data = data.rename(columns={'Odor': 'Odorant'})
+data.head()
+|
+# Write to disk
+molecules.to_csv('molecules.csv')
+stimuli.to_csv('stimuli.csv')
+subjects.to_csv('subjects.csv')
+screening.to_csv('behavior_1.csv')
 data.to_csv('behavior_2.csv')
+
