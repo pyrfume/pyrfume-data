1e3f4076a71e3201e8122d3828ef39b89dd83bf7
Travis Gould <52669531+TravisGould@users.noreply.github.com>
2022-10-19 17:54:47 -0400

    Arctander1960standardized (#116)
    * standardization updates
    
    Co-authored-by: Travis Gould <52669531+TGouldBates@users.noreply.github.com>

--- arctander_1960/main.py
+++ arctander_1960/main.py
@@ -1,25 +1,12 @@
-# -*- coding: utf-8 -*-
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.10.3
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
 import pandas as pd
 from pyrfume.odorants import get_cids, from_cids, canonical_smiles
 
+# Load raw data
 data = pd.read_csv('arctander.csv')
 
-# +
 data['SMILES'] = data['SMILES'].apply(lambda x: canonical_smiles(x) if isinstance(x, str) else None).str.strip()
 
 def replace(s):
@@ -29,7 +16,6 @@ def replace(s):
 
 data['ChemicalName'] = data['ChemicalName'].apply(lambda x: x if isinstance(x, str) else None).str.strip().apply(replace)
 data.head()
-# -
 
 cid_col = 'new_CID'  # alternative is 'CID', which has fewer entries
 data[cid_col] = data[cid_col].fillna(0)
@@ -38,44 +24,42 @@ needs_cids = data[data[cid_col]<=0].copy()
 len(needs_cids)
 
 cas_no_cids = needs_cids[needs_cids['CAS'].notnull()]['CAS'].str.strip()
-cas_no_cids.to_csv('untracked/cas_no_cids.txt', header=None, index=False)
-cas_no_cids  # Only one, aluminum sulfide solution (SID: 347706187)
+cas_no_cids  # Only one, ammonium sulfide solution (SID: 347706187)
 
-# +
 # Pointless if there are few CAS and all are known to not have CIDs (e.g. solutions not compounds)
-# cids_from_cas = pd.read_csv('cids_from_cas.txt', sep='\t', index_col=0, header=None)[1]
-# needs_cids.loc[cas_no_cids.index, cid_col] = cas_no_cids.apply(cids_from_cas.get, args=(0,)).fillna(0)
-# (needs_cids[cid_col]==0).sum()
-# -
 
-names_no_cids = needs_cids[needs_cids['CAS'].isnull() & 
-                           needs_cids['ChemicalName'].notnull()]['ChemicalName']
-names_no_cids.to_csv('untracked/names_no_cids.txt', header=None, index=False)
+# Try to find missing CIDs form names
+names_no_cids = needs_cids[(needs_cids[cid_col] == 0) & (needs_cids['ChemicalName'].notnull())]['ChemicalName']
+
 len(names_no_cids)
 
-# +
-# Use PubChem Exchange to go from names_no_cids.txt to cids_from_names.txt
-# -
+cids_from_names = get_cids(names_no_cids.to_list(), kind='name', verbose=False)
+print('%s found by name' % len([k for k, v in cids_from_names.items() if v]))
 
-cids_from_names = pd.read_csv('untracked/cids_from_names.txt', sep='\t', index_col=0, header=None)[1].dropna().astype(int)
-needs_cids.loc[names_no_cids.index, cid_col] = names_no_cids.apply(cids_from_names.get, args=(0,)).fillna(0)
+needs_cids.loc[names_no_cids.index, cid_col] = needs_cids.loc[names_no_cids.index, 'ChemicalName'].map(cids_from_names)
 (needs_cids[cid_col]==0).sum()
 
-smiles_no_cids = needs_cids[needs_cids['CAS'].isnull() & 
-                            needs_cids['ChemicalName'].isnull() &
-                            needs_cids['SMILES'].notnull()]['SMILES']
-smiles_no_cids.to_csv('untracked/smiles_no_cids.txt', header=None, index=False)
+# Try to find missing CIDs by SMILES
+smiles_no_cids = needs_cids[(needs_cids[cid_col] == 0)
+                            & (needs_cids['SMILES'].notnull())
+                            & (needs_cids['SMILES'] != '')]['SMILES']
+
 len(smiles_no_cids)
 
-# +
-# Use PubChem Exchange to go from smiles_no_cids.txt to cids_from_smiles.txt
+cids_from_smiles = get_cids(smiles_no_cids.to_list(), kind='SMILES', verbose=False)
+print('%s found by SMILES' % len([k for k, v in cids_from_smiles.items() if v]))
 
-# +
-# Pointless if there are no remaining SMILES without CIDs
-#cids_from_smiles = pd.read_csv('cids_from_smiles.txt', sep='\t', index_col=0, header=None)[1].dropna().astype(int)
-#needs_cids.loc[smiles_no_cids.index, cid_col] = smiles_no_cids.apply(cids_from_smiles.get, args=(0,)).fillna(0)
-#(needs_cids[cid_col]==0).sum()
-# -
+needs_cids.loc[smiles_no_cids.index, cid_col] = needs_cids.loc[smiles_no_cids.index, 'SMILES'].map(cids_from_smiles)
+(needs_cids[cid_col]==0).sum()
+
+# Try to find missing CIDs by InChI Key
+inchikey_no_cids = needs_cids[(needs_cids[cid_col] == 0) & needs_cids['InChiKey'].notnull()]['InChiKey']
+len(inchikey_no_cids)
+
+cids_from_inchikey = get_cids(inchikey_no_cids.to_list(), kind='inchikey', verbose=False)
+print('%s found by InChI Key' % len([k for k, v in cids_from_inchikey.items() if v]))
+
+# None found by InChI Key
 
 # Set the CIDSs that were found
 data.loc[needs_cids.index, cid_col] = needs_cids[cid_col].fillna(0)
@@ -83,27 +67,32 @@ cids = list(set(data[cid_col].values) - set([0]))
 
 molecules = pd.DataFrame(from_cids(cids)).set_index('CID').sort_index()
 
-identifiers = data[['ChemicalName', 'CAS', cid_col, 'ArctanderNum']].set_index('ArctanderNum')
-identifiers = identifiers.sort_index()
-identifiers.index.name = 'Stimulus'
-identifiers.head()
+molecules.head()
 
-molecules.to_csv('molecules.csv')
-identifiers.to_csv('identifiers.csv')
+stimuli = data[['ChemicalName', 'CAS', cid_col, 'ArctanderNum']].set_index('ArctanderNum')
+stimuli = stimuli.sort_index()
+stimuli.index.name = 'Stimulus'
+stimuli.head()
 
 sparse = data.set_index('ArctanderNum')['ChastretteDetails'].fillna('').str.split()
 sparse.index.name = 'Stimulus'
-sparse.colums = 'Labels'
-sparse.to_csv('behavior_1_sparse.csv')
+sparse.name = 'Labels'
+pd.DataFrame(sparse).head()
 
 all_labels = set.union(*sparse.apply(set).values)
 dense = pd.DataFrame(index=sparse.index, columns=all_labels)
 dense = dense.apply(lambda x: pd.Series({label: label in sparse[x.name] for label in x.index}),
                     axis=1)
 dense = dense.astype(int).sort_index(axis=1)
-dense.to_csv('behavior_1.csv')
 dense.head()
 
 desc = data.set_index('ArctanderNum')[['Description']]
 desc.index.name = 'Stimulus'
+desc.head()
+
+# Write to disk
+molecules.to_csv('molecules.csv')
+stimuli.to_csv('stimuli.csv')
+dense.to_csv('behavior_1.csv')
+sparse.to_csv('behavior_1_sparse.csv')
 desc.to_csv('behavior_2.csv')
