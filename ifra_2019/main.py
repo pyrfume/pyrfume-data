cf638cfb08b2688400f1fc133e24cdb9e77ba8a0
Travis Gould <52669531+TravisGould@users.noreply.github.com>
2022-10-19 17:51:46 -0400

    Ifra2019standardization (#125)
    * standardization updates
    
    * fixed molecules index
    
    Co-authored-by: Travis Gould <52669531+TGouldBates@users.noreply.github.com>

--- ifra_2019/main.py
+++ ifra_2019/main.py
@@ -1,17 +1,5 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.10.3
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
 import pandas as pd
 from pyrfume.odorants import get_cids, from_cids
@@ -19,17 +7,29 @@ from pyrfume.odorants import get_cids, from_cids
 raw = pd.read_csv('ifra-fragrance-ingredient-glossary---oct-2019.csv')
 cas = raw['CAS number']
 
-# + jupyter={"outputs_hidden": true} tags=[]
 cids = get_cids(cas)
-# -
 
 raw['CID'] = raw['CAS number'].apply(cids.get)
 raw = raw[raw['CID'].notnull() & raw['CID']>0]
 
-molecules = pd.DataFrame(from_cids(raw['CID'])).set_index('CID')
-molecules.to_csv('molecules.csv')
+molecules = pd.DataFrame(from_cids(raw['CID'])).set_index('CID').sort_index()
+
+molecules = molecules[~molecules.index.duplicated()] # Remove duplicates
+molecules.head()
 
 columns = ['CID', 'Primary descriptor', 'Descriptor 2', 'Descriptor 3']
 behavior = raw[columns].set_index('CID')
 behavior = behavior.rename(columns={'Primary descriptor': 'Descriptor 1'})
+behavior.index.name = 'Stimulus'
+behavior.head()
+
+# Create dataframe for stimuli.csv; all simuli are CIDs
+stimuli = pd.DataFrame(molecules.index.copy(), index=molecules.index.copy())
+stimuli.index.name = 'Stimulus'
+stimuli.head()
+
+# Write to disk
+molecules.to_csv('molecules.csv')
 behavior.to_csv('behavior.csv')
+stimuli.to_csv('stimuli.csv')
+
