0d71152ea7ae99f7b9c8a775605516a3a6eb22b6
Travis Gould <52669531+TravisGould@users.noreply.github.com>
2022-10-19 15:22:22 -0400

    Manoel2021standardized (#131)
    * standardization edits
    
    * Update manifest.toml
    
    * fixed molecules index
    
    Co-authored-by: Travis Gould <52669531+TGouldBates@users.noreply.github.com>

--- manoel_2021/main.py
+++ manoel_2021/main.py
@@ -1,44 +1,41 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.10.3
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
 # # Preprocessing for Manoel et al, 2021
 
 import pandas as pd
 from pyrfume.odorants import from_cids, cids_to_smiles
 
-# +
 # Load data about the odorants (names and PubChem IDs)
-rosetta = pd.read_excel("SmilesInfo2.xlsx")
+rosetta = pd.read_excel("SmilesInfo2.xlsx", engine='openpyxl')
 threeletter_to_pubchem = dict(rosetta.iloc[:, -1:-3:-1].values)
 threeletter_to_pubchem["MEN"] = threeletter_to_pubchem["+MEN"]
 threeletter_to_pubchem = pd.Series(threeletter_to_pubchem, name='CID').to_frame()
 
 # Load raw mouse data (individual mouse level)
-raw = pd.read_csv(
-    "raw behavioral scores mouse 73 odorants.csv", index_col=0, header=1
-).dropna()
+raw = pd.read_csv("raw_behavioral_scores_mouse_73_odorants.csv", index_col=0, header=1).dropna()
 raw.index.name = "odor"
 raw = raw.join(threeletter_to_pubchem).sort_index()
 print("Raw data has %d mouse/odorant combos" % raw.shape[0])
-# -
+raw.head()
 
 cids = raw['CID'].unique()
 smiles = cids_to_smiles(cids)
 
-molecules = pd.DataFrame(from_cids(cids))
-molecules.to_csv('molecules.csv')
+molecules = pd.DataFrame(from_cids(cids)).set_index('CID').sort_index()
+molecules.head()
 
-behavior = raw.set_index('CID').join(pd.Series(smiles, name='SMILES')).set_index('SMILES')
+behavior = raw.set_index('CID').sort_index().copy()
+behavior.index.name = 'Stimulus'
+behavior.head()
+
+# Create dataframe for stimuli.csv; all simuli are CIDs
+stimuli = pd.DataFrame(molecules.index.copy(), index=molecules.index.copy())
+stimuli.index.name = 'Stimulus'
+stimuli.head()
+
+# Write to disk
+molecules.to_csv('molecules.csv')
 behavior.to_csv('behavior.csv')
+stimuli.to_csv('stimuli.csv')
+
