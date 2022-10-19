1277e5bb8553a1b89afa5818bceb6cc3d2ae41a8
Travis Gould <52669531+TravisGould@users.noreply.github.com>
2022-10-19 15:08:00 -0400

    Bushdid2014standardization (#143)
    * bushdid2014standardization
    
    Co-authored-by: Travis Gould <52669531+TGouldBates@users.noreply.github.com>

--- bushdid_2014/main.py
+++ bushdid_2014/main.py
@@ -1,17 +1,5 @@
-# ---
-# jupyter:
-#   jupytext:
-#     formats: ipynb,py
-#     text_representation:
-#       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.10.3
-#   kernelspec:
-#     display_name: Python 3
-#     language: python
-#     name: python3
-# ---
+#!/usr/bin/env python
+# coding: utf-8
 
 # # Pyrfume data processing pipeline for Bushdid et al, 2014
 
@@ -23,7 +11,7 @@ from pyrfume.odorants import get_cids, from_cids
 # Load and trim white space
 s1 = pd.read_csv('tableS1.csv', encoding='latin1').iloc[:, :4].dropna()
 s1['Odorant name'] = s1['Odorant name'].apply(str.strip)
-s1.head()
+s1.tail()
 
 # Get PubChem IDs from CAS registry numbers
 cids = get_cids(s1['C.A.S.'])
@@ -42,35 +30,37 @@ cids.update({
 cids = pd.Series(cids, name='CID').astype(int)
 
 # Add the CIDs into the Table S1 dataframe
-s1 = s1.join(cids, on='C.A.S.').set_index('CID')
+s1 = s1.join(cids, on='C.A.S.').set_index('CID').sort_index()
 s1 = s1.replace('110-01-1', '110-01-0') # Fix thiolane CID
 s1.head()
 
 # Get standardized information for these molecules
 info = from_cids(cids.values)
 
-# Join data and save.
+# Join data and save
 molecules = pd.DataFrame(info).set_index('CID').join(s1)
-molecules.to_csv('molecules.csv')
+molecules = molecules[~molecules.index.duplicated()]
+molecules.head()
 
 # ### Supplementary Table S2 containing information about the mixtures and the triangle tests that used them
 
 s2 = pd.read_csv('tableS2.csv', encoding='latin1')
 s2 = s2[s2['Test UID'].str.isnumeric() > 0]  # Keep only numeric Test UIDs
 s2['Test UID'] = s2['Test UID'].astype(int)
+s2.tail(20)
 
-# +
 # Extract only the trial results for each Test UID and subject
-behavior = pd.melt(s2, id_vars=['Test UID'], value_vars=['subject %d' % i for i in range(1, 27)], var_name='Subject', value_name='Correct')
+behavior = pd.melt(s2, id_vars=['Test UID'],
+                   value_vars=['subject %d' % i for i in range(1, 27)],
+                   var_name='Subject', value_name='Correct')
+
 behavior = behavior.replace({'subject %d' % i: i for i in range(1, 27)})
-behavior = behavior.dropna().set_index(['Test UID', 'Subject']).sort_index()
+behavior.rename(columns={'Test UID': 'Stimulus'}, inplace=True)
+behavior = behavior.dropna().set_index(['Stimulus', 'Subject']).sort_index()
 behavior = behavior.replace({'wrong': False, 'right': True}).astype(bool)
 
-# Save behavioral data
-behavior.to_csv('behavior.csv')
 behavior.head()
 
-# +
 # Get all the trial metadata (including mixture composition)
 mixtures = s2.loc[:, :'subject 1'].iloc[:, :-1]
 mixtures = mixtures.rename(columns={'Unnamed: %d' % i: 'Molecule %d' % (i-5) for i in range(6, 36)})
@@ -79,6 +69,7 @@ mixtures = mixtures.rename(columns={'Unnamed: %d' % i: 'Molecule %d' % (i-5) for
 cid_map = {v.lower():k for k,v in molecules['Odorant name'].items()}
 mol_columns = ["Molecule %d" % i for i in range(1, 31)]
 mixtures[mol_columns] = mixtures[mol_columns].apply(lambda x: x.str.lower()).fillna(0)
+
 # Manual fixes
 mixtures = mixtures.replace(
     {'\xa01-isopropyl-4-methylbenzene': '1-isopropyl-4-methylbenzene',
@@ -89,7 +80,8 @@ mixtures[mol_columns] = mixtures[mol_columns].astype(int)
 # Cleanup other names
 mixtures['Stimulus dilution'] = mixtures['Stimulus dilution'].replace({'1/4': 0.25, '1/2': 0.5, 'not diluted': 1})
 mixtures[['Components in mixtures', 'Components that differ']] = mixtures[['Components in mixtures', 'Components that differ']].astype(int)
-mixtures = mixtures.set_index(['Test UID', 'Answer']).sort_index()
+mixtures.rename(columns={'Test UID': 'Stimulus'}, inplace=True)
+mixtures = mixtures.set_index(['Stimulus', 'Answer']).sort_index()
 
 # Sanity check
 for i, answer in enumerate(mixtures.index.get_level_values('Answer')):
@@ -97,10 +89,11 @@ for i, answer in enumerate(mixtures.index.get_level_values('Answer')):
         assert answer=='right'
     if i % 3 != 0:
         assert answer=='wrong'
+
 mixtures.head(9)
-# -
 
-# Save mixtures data
-mixtures.to_csv('mixtures.csv')
-
+# Write to disk
+molecules.to_csv('molecules.csv')
+mixtures.to_csv('stimuli.csv')
+behavior.to_csv('behavior.csv')
 
